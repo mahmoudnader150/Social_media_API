0fd5f4b3701977bc2b95cff2d9567c136532e2ce
mahmoudnader150 <mahnader222@gmail.com>
2025-02-15 09:56:55 +0200

    GET methods for user

--- social_media/src/main/java/com/example/social_media/user/UserDaoService.java
+++ social_media/src/main/java/com/example/social_media/user/UserDaoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class UserDaoService {
@@ -18,6 +19,11 @@ public class UserDaoService {
 
    public List<User> findAll() {
        return users;
+   }
+
+   public User findOne(int id) {
+       Predicate<? super User> predicate = user -> user.getId() == id;
+       return users.stream().filter(predicate).findFirst().get();
    }
 
 }
--- social_media/src/main/java/com/example/social_media/user/UserResource.java
+++ social_media/src/main/java/com/example/social_media/user/UserResource.java
@@ -1,5 +1,7 @@
 package com.example.social_media.user;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,7 +14,13 @@ public class UserResource {
         this.userDaoService = userDaoService;
     }
 
+    @GetMapping("/users")
     public List<User> retrieveAllUsers(){
           return userDaoService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id){
+      return userDaoService.findOne(id);
     }
 }
