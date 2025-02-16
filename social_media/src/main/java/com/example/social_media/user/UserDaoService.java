b2ef780343543b67b43b6afaa53ed451f6aa7ef1
mahmoudnader150 <mahnader222@gmail.com>
2025-02-16 13:51:24 +0200

    Post method for user

--- social_media/src/main/java/com/example/social_media/user/UserDaoService.java
+++ social_media/src/main/java/com/example/social_media/user/UserDaoService.java
@@ -11,10 +11,12 @@ import java.util.function.Predicate;
 public class UserDaoService {
    private static List<User> users = new ArrayList<>();
 
+   private static int usersCount = 0;
+
    static {
-       users.add(new User(1,"Adam", LocalDateTime.now().minusYears(30)));
-       users.add(new User(2,"Eve", LocalDateTime.now().minusYears(25)));
-       users.add(new User(3,"Jim", LocalDateTime.now().minusYears(20)));
+       users.add(new User(++usersCount,"Adam", LocalDateTime.now().minusYears(30)));
+       users.add(new User(++usersCount,"Eve", LocalDateTime.now().minusYears(25)));
+       users.add(new User(++usersCount,"Jim", LocalDateTime.now().minusYears(20)));
    }
 
    public List<User> findAll() {
@@ -24,6 +26,12 @@ public class UserDaoService {
    public User findOne(int id) {
        Predicate<? super User> predicate = user -> user.getId() == id;
        return users.stream().filter(predicate).findFirst().get();
+   }
+
+   public User save(User user) {
+       user.setId(++usersCount);
+       users.add(user);
+       return user;
    }
 
 }
--- social_media/src/main/java/com/example/social_media/user/UserResource.java
+++ social_media/src/main/java/com/example/social_media/user/UserResource.java
@@ -1,9 +1,10 @@
 package com.example.social_media.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,5 +23,16 @@ public class UserResource {
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id){
       return userDaoService.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User savedUser = userDaoService.save(user);
+        // return -> users/4
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
