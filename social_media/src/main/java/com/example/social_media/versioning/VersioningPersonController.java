7599745e6696390abdc5f5cc77410c1a560142c7
mahmoudnader150 <mahnader222@gmail.com>
2025-02-18 19:44:07 +0200

    Other Versioning methods

--- social_media/src/main/java/com/example/social_media/versioning/VersioningPersonController.java
+++ social_media/src/main/java/com/example/social_media/versioning/VersioningPersonController.java
@@ -1,6 +1,5 @@
 package com.example.social_media.versioning;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +13,36 @@ public class VersioningPersonController {
 
     @GetMapping("/v2/person")
     public PersonV2 getSecondVersionOfPerson(){
+        return new PersonV2(new Name("Mahmoud","Nader"));
+    }
+
+    @GetMapping(path="/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter(){
+        return new PersonV1("Mahmoud Nader");
+    }
+
+    @GetMapping(path="/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter(){
+        return new PersonV2(new Name("Mahmoud","Nader"));
+    }
+
+    @GetMapping(path="/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonHeaders(){
+        return new PersonV1("Mahmoud Nader");
+    }
+
+    @GetMapping(path="/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonHeaders(){
+        return new PersonV2(new Name("Mahmoud","Nader"));
+    }
+
+    @GetMapping(path="/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeaders(){
+        return new PersonV1("Mahmoud Nader");
+    }
+
+    @GetMapping(path="/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionOfPersonAcceptHeaders(){
         return new PersonV2(new Name("Mahmoud","Nader"));
     }
 
